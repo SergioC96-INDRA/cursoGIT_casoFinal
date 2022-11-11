1fb490cc656518a5686eb894b5b1fa12f30db5f6
[Roman Soroka] <[rsoroka@indra.es]>
2022-11-11 12:56:49 +0100

    add all files from caso_1_2_3

---
+++ hello_world.adb
@@ -0,0 +1,13 @@
+--HELLO WORLD
+--Authors: Sergio, Santiago, Roman
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+package body First_program is
+
+  procedure Hello_World is
+  begin
+  	Put_Line("Hello World");
+  end Hello_World;
+
+end First_program;
---
+++ hola_mundo.ps1
@@ -0,0 +1,7 @@
+Write-Host "Hola Mundo" 
+
+Write-Host "|---------|"
+Write-Host "| O     O |"
+Write-Host "|    ^    |"
+Write-Host "| >>>>>>> |"
+Write-Host "|---------|"
